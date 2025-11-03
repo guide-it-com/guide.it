f4dd691e59a191584cab95307a0b713f0200e3da
TexKiller <3438226+TexKiller@users.noreply.github.com>
2025-11-03 16:46:06 -0300

    feat: support Linux through Electron

---
+++ linux/src/preload.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports, no-undef
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  on: (channel, callback) => {
+    ipcRenderer.on(channel, callback);
+  },
+  send: (channel, args) => {
+    ipcRenderer.send(channel, args);
+  },
+});
